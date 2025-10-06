ca1d65f584215419be7c65b99f53195562d5de73
peonymyx <peonymyx@gmail.com>
2025-10-06 23:19:19 +0700

    SPT-10 Create Github Environment

--- src/auth/helpers/is-token-valid.helper.ts
+++
@@ -1,9 +0,0 @@
-export function isTokenValid(
-  token: string,
-  expiry: number,
-  now = Date.now(),
-): boolean {
-  console.log("expiry: ", expiry);
-  console.log("now: ", now);
-  return !!token && expiry > now;
-}
